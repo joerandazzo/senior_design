2b495921f06bdf988ae1d0b67d57b6dc5c9b94ae
Joseph Randazzo <jrandazzo@coolfiresolutions.com>
2018-04-14 16:14:31 -0500

    Lifecycle bluetooth connection Orientation

--- android/app/src/main/java/edu/siue/mech/seniordesign/fragment/SettingsBluetoothFragment.java
+++ android/app/src/main/java/edu/siue/mech/seniordesign/fragment/SettingsBluetoothFragment.java
@@ -1,15 +1,25 @@
 package edu.siue.mech.seniordesign.fragment;
 
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Set;
 
 import edu.siue.mech.seniordesign.R;
 
 public class SettingsBluetoothFragment extends Fragment {
+
+    private static final String TAG = SettingsBluetoothFragment.class.getSimpleName();
+    BluetoothAdapter bluetoothAdapter;
 
     public static SettingsBluetoothFragment newInstance() {
         return new SettingsBluetoothFragment();
@@ -22,11 +32,48 @@ public class SettingsBluetoothFragment extends Fragment {
     @Override
     public void onActivityCreated(Bundle savedInstance){
         super.onActivityCreated(savedInstance);
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         initUI();
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        getPairedDevice();
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
     }
 
     private void initUI(){
         View view = getView();
 
     }
+
+    private void getPairedDevice(){
+        // Get a set of currently paired devices
+        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+        if(pairedDevices == null){
+            return;
+        }
+
+        BluetoothDevice foundDevice = null;
+        for(BluetoothDevice device : pairedDevices){
+            Log.d(TAG, device.getName() + device.getAddress());
+            if(device.getAddress().contains("21:13:01:93:79")){
+                Log.d(TAG, "FOUND");
+                foundDevice = device;
+            }
+        }
+        if(foundDevice != null){
+            ((ImageView)getActivity().findViewById(R.id.ivStatus)).setImageResource(R.drawable.shape_circle_green);
+            ((TextView)getActivity().findViewById(R.id.tvName)).setText(foundDevice.getName());
+        }else{
+            ((ImageView)getActivity().findViewById(R.id.ivStatus)).setImageResource(R.drawable.shape_circle_red);
+            ((TextView)getActivity().findViewById(R.id.tvName)).setText("Not found");
+        }
+    }
+
 }
