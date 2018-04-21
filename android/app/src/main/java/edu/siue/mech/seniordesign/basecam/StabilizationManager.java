c22e9d25f58579e428711855a7795987b262f326
Grant Richard Steiner <grantsteiner64@gmail.com>
2018-04-21 13:28:54 -0500

    added body checksum, constructed packet

--- android/app/src/main/java/edu/siue/mech/seniordesign/basecam/StabilizationManager.java
+++ android/app/src/main/java/edu/siue/mech/seniordesign/basecam/StabilizationManager.java
@@ -33,13 +33,14 @@ public class StabilizationManager {
         short yaw_i = (short)yaw;
         short pitch_i = (short)pitch;
         short roll_i = (short)roll;
-
         // Speeds set to 0 so they are controlled by board. Idea is that the time increments are
         // small enough that it might not be noticeable
         byte [] values = new byte[]{(byte)(02 & 0xFF), //Control code for MODE_ANGLE
                 (byte)((roll_i & 0xFF00)>>8), (byte)(roll_i & 0x00FF), 0x00,  0x00, //Roll angle, split into two byte component, and two bytes for speed
                 (byte)((pitch_i & 0xFF00)>>8), (byte)(pitch_i & 0x00FF), 0x00, 0x00, //Pitch angle, split into two byte components, and two bytes for speed
                 (byte)((yaw_i & 0xFF00)>>8), (byte)(yaw_i & 0x00FF), 0x00, 0x00}; //Yaw angle, split into two byte components, and two bytes for speed
+
+        sendData(CMD.CONTROL.value, values);
     }
 
     public void sendData(int command, byte[] values){
@@ -59,43 +60,41 @@ public class StabilizationManager {
 
     //Packet definition on page 3 of serial API docs
     private static byte[] getPacket(int commandId, byte[] values){
-        byte[] packet = new byte[6];
-        byte[] dataSize = new byte[0];
-        int modulus = (byte) ((commandId + dataSize.length) % 256);
-
-        if(commandId == 77) {  //Construct MOTORS_OFF Command
+        if(commandId == 77 || commandId == 109) {  //Construct MOTORS_OFF/ON Command
+            byte[] packet = new byte[6];
+            byte[] dataSize = new byte[0];
+            int modulus = (byte) ((commandId + dataSize.length) % 256);
             packet[0] = 62; //Start character
             packet[1] = ((byte) (commandId & 0xFF)); //Command ID (Unsigned)
             packet[2] = ((byte) (dataSize.length & 0xFF)); //Data size (Unsigned)
             packet[3] = ((byte) (modulus & 0xFF)); //Header checksum (Unsigned)
-        } else if (commandId == 109) { //Construct MOTORS_ON Command
-            packet[0] = 62; //Start character
-            packet[1] = ((byte) (commandId & 0xFF)); //Command ID (Unsigned)
-            packet[2] = ((byte) (dataSize.length & 0xFF)); //Data size (Unsigned)
-            packet[3] = ((byte) (modulus & 0xFF)); //Header Checksum (Unsigned)
+            return packet;
         } else if (commandId == 67) { //Construct CONTROL Command
+            byte[] packet = new byte[18];
+            int modulus = (byte) ((commandId + values.length) % 256);
+            packet[0] = 62; //Start character
+            packet[1] = ((byte) (commandId & 0xFF)); //Command ID (Unsigned)
+            packet[2] = ((byte) (values.length & 0xFF)); //Data size (Unsigned)
+            packet[3] = ((byte) (modulus & 0xFF)); //Header checksum (Unsigned)
+
+            byte checksum = 0;
+            for (int i=4; i<values.length+4; i++){
+                packet[i] = values[i-4];
+                checksum += values[i-4];
+            }
+
+            packet[17] = (byte)((checksum % 256) & 0xFF);
+
+            return packet;
+        } else { // If we get an unrecognized command, we shut the motors off -> not sure this is the best option
+            byte[] packet = new byte[6];
+            byte[] dataSize = new byte[0];
+            int modulus = (byte) ((77 + dataSize.length) % 256);
             packet[0] = 62; //Start character
             packet[1] = ((byte) (commandId & 0xFF)); //Command ID (Unsigned)
             packet[2] = ((byte) (dataSize.length & 0xFF)); //Data size (Unsigned)
             packet[3] = ((byte) (modulus & 0xFF)); //Header checksum (Unsigned)
-            //packet[4] = CONTROL_MODE (Unsigned)
-            //packet[5] = first byte in ROLL_SPEED (Signed)
-            //packet[6] = second "    "  "     "      "
-            //packet[7] = first byte in ROLL_ANGLE (Signed)
-            //packet[8] = second "    "   "    "      "
-            //packet[9] = first byte in PITCH_SPEED (Signed)
-            //packet[10] = second "    "  "     "      "
-            //packet[11] = first byte in PITCH_ANGLE (Signed)
-            //packet[12] = second "    "   "    "      "
-            //packet[13] = first byte in YAW_SPEED (Signed)
-            //packet[14] = second "    "  "     "      "
-            //packet[15] = first byte in YAW_ANGLE (Signed)
-            //packet[16] = second "    "   "    "      "
-            //packet[17] = body checksum (unsigned)
-
+            return packet;
         }
-        // packet[4] = ((byte) (values.length & 0xFF));
-        //packet[5] = ??
-        return packet;
     }
 }
